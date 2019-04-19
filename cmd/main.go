d501d50fdf83110af05178327af5a2b742c10d43
Oleg Fedorov <olfedorov@ozon.ru>
2019-04-19 14:20:23 +0300

    init commit #2

--- cmd/main.go
+++ cmd/main.go
@@ -4,25 +4,38 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"sync"
+	"sync/atomic"
 )
 
-func main() {
-	url := "https://goodster.ru/cat/61803/"
-	fmt.Println("request: " + url)
+const startID = 1000000
+const path = "https://goodster.ru"
 
-	//fmt.Println(parseCatalogPage(url))
-	pararellParser([]string{url})
+var ID = int64(startID)
+
+type parseUrl struct {
+	url   string
+	level int
 }
 
-func parseCatalogPage(url string) []string {
-	var answer []string
+func main() {
+	startCategories := getStartCategories()
+	pararellParser(startCategories)
+}
 
-	doc, err := goquery.NewDocument(url)
+func getID() int64 {
+	return atomic.AddInt64(&ID, int64(1))
+}
+
+func parseCatalogPage(url parseUrl) []parseUrl {
+	var answer []parseUrl
+
+	doc, err := goquery.NewDocument(path + url.url)
 	if err != nil {
 		panic(err)
 	}
 
 	doc.Find("li.categoriesListBoxTitle").Each(func(i int, s *goquery.Selection) {
+		id := getID()
 		node := s.Find("a").Eq(0)
 		href, ok := node.Attr("href")
 		if !ok {
@@ -32,21 +45,80 @@ func parseCatalogPage(url string) []string {
 		if err != nil {
 			panic(err)
 		}
-		answer = append(answer, href)
-		fmt.Println(title, href)
+		answer = append(answer, parseUrl{
+			url:   href,
+			level: url.level + 1,
+		})
+		fmt.Printf(`{id: %d, title: "%s", href: "%s", lvl: %d}`, id, title, path+href, url.level+1)
+		fmt.Println()
 	})
 
 	return answer
 }
 
-func pararellParser(urls []string) {
+func pararellParser(urls []parseUrl) {
 	wg := sync.WaitGroup{}
 	wg.Add(len(urls))
 	for _, url := range urls {
-		go func() {
+		go func(url parseUrl) {
 			pararellParser(parseCatalogPage(url))
 			wg.Done()
-		}()
+		}(url)
 	}
 	wg.Wait()
+}
+
+func getStartCategories() []parseUrl {
+	var answer []parseUrl
+
+	//+ "/tools/ajax/mainNav.php"
+	doc, err := goquery.NewDocument(path)
+	if err != nil {
+		panic(err)
+	}
+
+	doc.Find(".mainNav .mainNavLevel1").Each(func(i int, s *goquery.Selection) {
+		id := getID()
+		node := s.Find("a").Eq(0)
+		href, ok := node.Attr("href")
+		if !ok {
+			panic("qwe")
+		}
+		if href == "#" {
+			//	s.Find(".mainNavTitle>a").Each(func(i int, s *goquery.Selection) {
+			//		href, ok := s.Attr("href")
+			//		if !ok {
+			//			panic("qwe")
+			//		}
+			//		title, err := s.Html()
+			//		if err != nil {
+			//			panic(err)
+			//		}
+			//
+			//		answer = append(answer, parseUrl{
+			//			url: href,
+			//			level: 1,
+			//		})
+			//
+			//		fmt.Printf(`{id: %d, title: "%s", href: "%s", lvl: %d}`, id, title, path + href, 1)
+			//		fmt.Println()
+			//	})
+			return
+		}
+
+		title, err := node.Html()
+		if err != nil {
+			panic(err)
+		}
+
+		answer = append(answer, parseUrl{
+			url:   href,
+			level: 1,
+		})
+
+		fmt.Printf(`{id: %d, title: "%s", href: "%s", lvl: %d}`, id, title, path+href, 1)
+		fmt.Println()
+	})
+
+	return answer
 }
